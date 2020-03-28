bc2da937564012bf7c00262d1369b279856ce08c
benauwe <edouard.benauw@student.ecp.fr>
2020-03-28 21:06:12 +0100

    Add blur and downsample wrappers

--- filters.py
+++ filters.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 from project_types import NumpyArray
@@ -16,3 +17,30 @@ def binomial_filter_2d(size: int = 2) -> NumpyArray:
     filter_1d = binomial_filter_1d(size)
     filter_2d = np.outer(filter_1d, filter_1d)
     return filter_2d
+
+
+def blur_and_downsample_all_channels(img: NumpyArray, filt: NumpyArray, level: int = 1) -> NumpyArray:
+    """ Blur and downsample all three channels of a HSV or RGB image, represented by a Numpy Matrix.
+          The blurring is done with the 2D filter kernel specified by filt, which has to be a Numpy Matrix.
+          The procedure will be applied recursively level times (default=1)."""
+    comp1, comp2, comp3 = cv2.split(img)
+    processed_comp1 = blur_and_downsample_one_channel(comp1, filt, level)
+    processed_comp2 = blur_and_downsample_one_channel(comp2, filt, level)
+    processed_comp3 = blur_and_downsample_one_channel(comp3, filt, level)
+    result = cv2.merge((processed_comp1, processed_comp2, processed_comp3))
+    return result
+
+
+def blur_and_downsample_one_channel(img: NumpyArray, filt: NumpyArray, level: int = 1) -> NumpyArray:
+    """ Blur and downsample one channel of image, represented by a Numpy Matrix.
+      The blurring is done with the 2D filter kernel specified by filt, which has to be a Numpy Matrix.
+      The procedure is applied recursively level times (default=1)."""
+    if level > 1:
+        img = blur_and_downsample_one_channel(img, level - 1, filt)
+
+    if level >= 1:
+        res = corrDn(img, filter, step=(2, 2))
+    else:
+        res = img
+
+    return res
