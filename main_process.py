edc6924e26cb0d7830907cd87e9089d844b8a093
benauwe <edouard.benauw@student.ecp.fr>
2020-03-29 15:08:21 +0200

    Implemented temporal filtering

---
+++ main_process.py
@@ -0,0 +1,31 @@
+import cv2
+from project_types import NumpyArray
+from spatial_processing import blur_and_downsample_video
+from temporal_processing import frequential_filter_video
+from video_helpers import convert_video_to_np_array, convert_np_array_to_video, \
+    get_width_and_height_from_frames
+import numpy as np
+
+
+def merge_with_processed(original_frames: NumpyArray, processed_frames: NumpyArray, alpha: float,
+                         chrome_attenuation: float) -> NumpyArray:
+    # Amplification
+    reduced_width, reduced_height = get_width_and_height_from_frames(processed_frames)
+    dest_width, dest_height = get_width_and_height_from_frames(original_frames)
+    total_time = processed_frames.shape[0]
+    processed_frames_amplified = np.zeros((total_time, reduced_height, reduced_width, 3))
+    processed_frames_amplified[:][:][:][0] = processed_frames[:][:][:][0] * alpha
+    processed_frames_amplified[:][:][:][1] = processed_frames[:][:][:][0] * alpha * chrome_attenuation
+    processed_frames_amplified[:][:][:][2] = processed_frames[:][:][:][0] * alpha * chrome_attenuation
+    # Merging
+    merged = original_frames + cv2.resize(processed_frames_amplified, (dest_height, dest_width))
+    return merged
+
+
+def process_video(source_path: str, out_path: str, downsample_level: int, lowcut: float, highcut: float, fs: float,
+                  order: int, alpha: float, chrome_attenuation: float) -> None:
+    frames, fps = convert_video_to_np_array(source_path)
+    spatially_processed_frames = blur_and_downsample_video(frames, downsample_level)
+    processed_frames = frequential_filter_video(spatially_processed_frames, lowcut, highcut, fs, order)
+    merged_frames = merge_with_processed(frames, processed_frames, alpha, chrome_attenuation)
+    convert_np_array_to_video(merged_frames, out_path, fps)
---
+++ temporal_processing.py
@@ -0,0 +1,46 @@
+import numpy as np
+from project_types import NumpyArray
+from scipy.signal import butter, sosfilt
+from video_helpers import get_width_and_height_from_frames
+
+"""https://scipy-cookbook.readthedocs.io/items/ButterworthBandpass.html"""
+
+
+def butter_bandpass(lowcut: float, highcut: float, fs: float, order=5) -> NumpyArray:
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = butter(order, [low, high], analog=False, btype='band', output='sos')
+    return sos
+
+
+def butter_bandpass_filter(data: NumpyArray, lowcut: float, highcut: float, fs: float, order=5) -> NumpyArray:
+    sos = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = sosfilt(sos, data)
+    return y
+
+
+def frequential_filter_video(spatially_processed_frames: NumpyArray, lowcut: float, highcut: float, fs: float, order=5) -> NumpyArray:
+    width, height = get_width_and_height_from_frames(spatially_processed_frames)
+    total_time = spatially_processed_frames.shape[0]
+    pre_processed_video = np.zeros((3, height, width, total_time))
+    post_processed_video = np.zeros((total_time, height, width, 3))
+    # Shape modification to ease frequential filtering
+    for t in range(total_time):
+        for i in range(height):
+            for j in range(width):
+                for color in range(3):
+                    pre_processed_video[color][i][j][t] = spatially_processed_frames[t][i][j][color]
+    # Filtering
+    for color in range(3):
+        for i in range(height):
+            for j in range(width):
+                data = pre_processed_video[color][i][j]
+                pre_processed_video[color][i][j] = butter_bandpass_filter(data, lowcut, highcut, fs, order=order)
+    # Shape modification to return to the original shape
+    for color in range(3):
+        for i in range(height):
+            for j in range(width):
+                for t in range(total_time):
+                    post_processed_video[t][i][j][color] = pre_processed_video[color][i][j][t]
+    return post_processed_video
